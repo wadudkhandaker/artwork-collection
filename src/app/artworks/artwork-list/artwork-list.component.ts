a1b02c0fc448a739bcbb4ef645206ac73c6b7a68
Md Abdul Wadud Khandaker <mdabdulwadudkhandaker@Mds-Laptop.local>
2024-07-06 18:40:05 +0700

    docs: apply compodoc in artwork list

--- src/app/artworks/artwork-list/artwork-list.component.ts
+++ src/app/artworks/artwork-list/artwork-list.component.ts
@@ -14,14 +14,40 @@ import { Observable } from 'rxjs';
   styleUrls: ['./artwork-list.component.scss']
 })
 export class ArtworkListComponent implements OnInit, OnChanges {
+  /**
+   * Filter criteria for artworks.
+   */
   @Input() filter: string[] = [];
+  
+  /**
+   * Sort criteria for artworks.
+   */
   @Input() sort: string = '';
 
+  /**
+   * Observable stream of artworks.
+   */
   artworks$: Observable<any[]>;
+  
+  /**
+   * Observable stream indicating the loading state.
+   */
   loading$: Observable<boolean>;
+  
+  /**
+   * Observable stream of any errors.
+   */
   error$: Observable<any>;
+  
+  /**
+   * Observable stream of pagination information.
+   */
   pagination$: Observable<any>;
 
+  /**
+   * Constructor for ArtworkListComponent.
+   * @param store - The NgRx store for state management.
+   */
   constructor(private store: Store<AppState>) {
     this.artworks$ = this.store.pipe(select(state => state[fromArtwork.artworkFeatureKey].filteredArtworks));
     this.loading$ = this.store.pipe(select(state => state[fromArtwork.artworkFeatureKey].loading));
@@ -29,20 +55,34 @@ export class ArtworkListComponent implements OnInit, OnChanges {
     this.pagination$ = this.store.pipe(select(state => state[fromArtwork.artworkFeatureKey].pagination));
   }
 
+  /**
+   * Initializes the component and dispatches the action to load artworks.
+   */
   ngOnInit(): void {
     this.store.dispatch(ArtworkActions.loadArtworks({ page: 1 }));
   }
 
+  /**
+   * Detects changes in input properties and applies the filter and sort criteria.
+   * @param changes - The changes in input properties.
+   */
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['filter'] || changes['sort']) {
       this.applyFilterSort();
     }
   }
 
+  /**
+   * Applies the filter and sort criteria by dispatching an action.
+   */
   applyFilterSort(): void {
     this.store.dispatch(ArtworkActions.filterArtworks({ filter: this.filter, sort: this.sort }));
   }
 
+  /**
+   * Handles page change events by dispatching an action to load the artworks for the selected page.
+   * @param page - The selected page number.
+   */
   onPageChange(page: number): void {
     this.store.dispatch(ArtworkActions.loadArtworks({ page }));
   }
